d6203ec5d9299f87b87230f0407e7ccf98b5bcb6
Divya12345678-ux <divyaruba6@gmail.com>
2026-02-13 10:08:14 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -0,0 +1,15 @@
+# Last updated: 2/13/2026, 10:08:13 AM
+from collections import Counter
+class Solution(object):
+    def longestPalindrome(self, s):
+        count = Counter(s)
+        length = 0
+        odd_found = False
+        for freq in count.values():
+            length += freq // 2 * 2
+            if freq % 2 == 1:
+                odd_found = True
+
+        if odd_found:
+            length += 1
+        return length
